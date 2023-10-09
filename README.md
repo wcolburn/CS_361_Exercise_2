87c5ac758fbe2131a8ee44c6420af50c8f11dea7
Liam Colburn <colburnw@oregonstate.edu>
2023-10-09 11:24:19 -0700

    Rename choose parameter and remove middleman statements to assign new name to @name

---
+++ README.md
@@ -0,0 +1,7 @@
+William Colburn
+CS_361
+Fall 2023
+
+Exercise 2: Functions & Comments
+
+This program is a refactor of previous code. It contains a function that reverses a name, and appends "Borg" to the name if specified.
--- main.rb
+++ main.rb
@@ -3,20 +3,16 @@
 @name = "Johanna Jackson"
 
 # Mangle the name by reversing it or borgifying it
-def modified_name(choose)
+def modified_name(appendBorg)
   # Split the name into first and last in an array
   split_name = @name.split
-  #reversed_twice = split_name.reverse.reverse
-  #mangled_name = reversed_twice << "Mangled"
-  #return mangled_name * 2
   # Reverse the array of name components so the array is [last, first]
   reversed_name_components = split_name.reverse
   # Rejoin the array of name components
-  new_name1 = reversed_name_components.join(' ')
+  @name = reversed_name_components.join(' ')
   # Store the mangled name
-  @name = new_name1
-  puts new_name1
-  if choose
+  puts @name
+  if appendBorg
     # Split the name into last and first in an array
     split_name2 = new_name1.split
     # Reverse the array of name components so the array is [last, first]
@@ -24,9 +20,8 @@ def modified_name(choose)
     # Add borg to the end of the array
     reversed_name_components2 << "Borg"
     # Rejoin the array of name components
-    new_name2 = reversed_name_components2.join(' ')
-    puts new_name2
-    @name = new_name2
+    @name = reversed_name_components2.join(' ')
+    puts @name
   end
   @name
 end
