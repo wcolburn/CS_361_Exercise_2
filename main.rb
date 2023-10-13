aed413a2220479dd343247218c286874e62e1f08
Liam Colburn <colburnw@oregonstate.edu>
2023-10-13 13:23:33 -0700

    Change function names to be more accurate

--- main.rb
+++ main.rb
@@ -1,23 +1,15 @@
 
 # Mangle the name by reversing it or borgifying it
-def modified_name(name, appendBorg)
+def create_reversed_name(name)
   split_name = name.split
   reversed_name_components = split_name.reverse
-  name = reversed_name_components.join(' ')
-
-  if appendBorg
-    split_name2 = name.split
-    reversed_name_components2 = split_name2.reverse
-    reversed_name_components2 << "Borg"
-    name = reversed_name_components2.join(' ')
-  end
-  name
+  reversed_name_components.join(' ')
 end
 
-def borg_name(name)
+def create_borg_name(name)
   name << " Borg"
 end
 
 @name = "Johanna Jackson"
-puts "New name: #{modified_name(@name,false)}"
-puts "New borg name: #{borg_name(@name)}"
+puts "New name: #{create_reversed_name(@name)}"
+puts "New borg name: #{create_borg_name(@name)}"
