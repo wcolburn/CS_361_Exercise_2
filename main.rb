3fe7e8a4d6af1049a4624b091d61b8f3a4288a20
Liam Colburn <colburnw@oregonstate.edu>
2023-10-13 13:17:25 -0700

    Remove global variable and replace with parameter for name) '

--- main.rb
+++ main.rb
@@ -1,20 +1,19 @@
 
-@name = "Johanna Jackson"
 
 # Mangle the name by reversing it or borgifying it
-def modified_name(appendBorg)
-  split_name = @name.split
+def modified_name(name, appendBorg)
+  split_name = name.split
   reversed_name_components = split_name.reverse
-  @name = reversed_name_components.join(' ')
+  name = reversed_name_components.join(' ')
 
   if appendBorg
-    split_name2 = @name.split
+    split_name2 = name.split
     reversed_name_components2 = split_name2.reverse
     reversed_name_components2 << "Borg"
-    @name = reversed_name_components2.join(' ')
+    name = reversed_name_components2.join(' ')
   end
-  @name
+  name
 end
 
-puts "New name: #{modified_name(false)}"
-puts "New borg name: #{modified_name(true)}"
+puts "New name: #{modified_name("Johanna Jackson",false)}"
+puts "New borg name: #{modified_name("Johanna Jackson",true)}"
